6066b0c4bb54ec97fd949a0b35a95d103277a026
Beardkoda <akinsuyi.joshua84@gmail.com>
2019-07-06 16:53:03 +0100

    Full update2

--- app/Http/Controllers/Admin/InvestmentController.php
+++ app/Http/Controllers/Admin/InvestmentController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\investment;
-use App\Product;
+use App\product;
 use App\Sponsor;
 
 class InvestmentController extends Controller
--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use App\Product;
+use App\product;
 use App\Sponsor;
 use App\Type;
 
